4baeb94ece34f3ec63dc6dda9f51d67a97a63565
Navalkar-Nikhil <120443609+Navalkar-Nikhil@users.noreply.github.com>
2023-08-09 00:39:36 +0530

    Update taskMiddleware.js

--- middleware/taskMiddleware.js
+++ middleware/taskMiddleware.js
@@ -13,13 +13,13 @@ Response
 
 1. Unabel to verify token (Invalid Token)
 404 Status Code
-json = 
+json =
 {
     status: 'fail',
     message: 'Invalid token'
 }
 
-2. if Given task_id does not exist in Tasks 
+2. if Given task_id does not exist in Tasks
 
 404 Status code
 
@@ -32,7 +32,7 @@ json = {
    means this is not the owner of given task hence
 
 403 Status code
-json = 
+json =
 {
     status: 'fail',
     message: 'Access Denied'
@@ -48,8 +48,42 @@ json =
 async function isowner(req, res, next) {
 
     try {
-        
+       
         //Write your code here.
+        const {task_id,token} = req.body;
+        let decodedToken,user_id;
+        try{
+            decodedToken = jwt.verify(token,JWT_SECRET);
+            user_id = decodedToken.userId;
+
+           
+        }catch(err){
+            res.status(404).json({
+                status:'fail',
+                message:'Invalid token'
+            })
+        }
+
+        try{
+         const task = await Tasks.findById(task_id);
+         if(String(task.creator_id) === user_id){
+            next();
+         }
+         else{
+            res.status(403).json({
+                status:'fail',
+                message:'Access Denied'
+            })
+         }
+
+        }catch(err){
+            res.status(404).json({
+                status:'fail',
+                message:'Given task doesnot exist'
+            })
+
+        }
+
 
     } catch (err) {
         return res.status(400).json({
